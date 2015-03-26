7dda318a8bc13a7de0ed2d4ecf7ab81a0e9362e4
wdk <wdk@erongdu.com>
2015-03-26 16:17:36 +0800

    建表语句

---
+++ src/main/resources/conf/createTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `USER` (
+`ID`  varchar(50) NOT NULL ,
+`NAME`  varchar(20) NULL ,
+`AGE`  int NULL ,
+`TEL`  varchar(15) NULL ,
+`SEX`  int NULL ,
+`ADDRESS`  varchar(200) NULL ,
+`BZ`  varchar(500) NULL ,
+PRIMARY KEY (`ID`)
+)
+;
